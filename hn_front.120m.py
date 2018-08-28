b9ad55ea12854ec5639e25e16068b309a62f16e7
amrrs <amrrs@outlook.com>
2018-08-28 09:22:09 +0530

    fixing connection check and execution chmod

--- hn_front.120m.py
+++ hn_front.120m.py
@@ -10,29 +10,20 @@
 # <bitbar.abouturl>https://github.com/amrrs/hn_headlines_bitbar/blob/master/hn_front.120m.py</bitbar.abouturl>
 
 
-import requests
+from requests import get, exceptions
+from sys import exit
 print("HN")
-#check if internet connection is available
-#@yasinkuyu 08/09/2017
-#source: https://gist.github.com/yasinkuyu/aa505c1f4bbb4016281d7167b8fa2fc2
-
-def check_internet():
-    url='http://www.google.com/'
-    timeout=5
-    try:
-        _ = requests.get(url, timeout=timeout)
-        return True
-    except requests.ConnectionError:
-        print('---')
-        print("Internet Connection Not available")
-    return False
-if check_internet():
-    content = requests.get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
-    ids = content.json()
-    story_base = "https://hacker-news.firebaseio.com/v0/item/"
-    hn_link = "https://news.ycombinator.com/item?id="
+try:
+    content = get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+except exceptions.RequestException:    
     print('---')
-    for id in ids[:10]:
-        story = requests.get(story_base + str(id) + ".json")
-        story_json = story.json()
-        print(story_json["title"] + "| href = https://news.ycombinator.com/item?id=" + str(id))
+    print("Internet Connection Not available")
+    exit(1)
+ids = content.json()
+story_base = "https://hacker-news.firebaseio.com/v0/item/"
+hn_link = "https://news.ycombinator.com/item?id="
+print('---')
+for id in ids[:10]:
+    story = get(story_base + str(id) + ".json")
+    story_json = story.json()
+    print(story_json["title"] + "| href = https://news.ycombinator.com/item?id=" + str(id))
